8313b5f11300558a58fa65ca0d1ee4ab6c38aba8
Shannon King <kingshannon01@gmail.com>
2019-03-06 23:34:34 -0800

    homework

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
-function getGifs(celebrities) {
+function getGifs(snlComics) {
     var apiKey = "jOiEr6XvZhLT9FxHSmErRdC6PjHSolFb";
-    var searchTerm = celebrities;
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=20&offset=0&rating=PG-13&lang=en";
+    var searchTerm = snlComics;
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=10&offset=0&rating=PG-13&lang=en";
 
     $.ajax({
         url: queryUrl,
@@ -9,31 +9,31 @@ function getGifs(celebrities) {
     })
         .then(function (response) {
             for (var i = 0; i < response.data.length; i++) {
-                //   below is where you left off getting to the point where you have your still and your animated... see activity Ajax, pausing gifs solution 
-                // console.log(response.data[i]);
-                // console.log(response.data[i].images.original.url);
                 var p = $("<p>");
                 p.text("Rated: " + response.data[i].rating);
-                var img = $("<img data-state='still'>");
-
+                var img = $("<img data-state='still' class='gif'>");
                 img.attr("src", response.data[i].images.original.url);
-                img.attr("data-moving-image", response.data[i].images.original.url);
-                img.attr("data-still-image", response.data[i].images.original_still.url);
-
+                img.attr("data-animate", response.data[i].images.original.url);
+                img.attr("data-still", response.data[i].images.original_still.url);
                 $("#gif-holder").append(p, img);
             }
         });
 }
 
-getGifs("");
+function newButtons() {
+    var btn = $("<button>");
+    $("#search-bar").val("");
+    getGifs("");
+}
 
 function makeButtons() {
-    var celebrities = ["Kate McKinnon", "Jim Carrey", "Kenan Thompson", "Rashida Jones", "Maya Rudolph", "Bill Murray",
+    var snlComics = ["Kate McKinnon", "Jim Carrey", "Kenan Thompson", "Rashida Jones", "Maya Rudolph", "Bill Murray",
         "Leslie Jones", "Amy Poehler", "Will Ferrell", "Cecily Strong", "Tiny Fey", "Julia Louis Dreyfus", "Billy Crystal"];
-    for (var i = 0; i < celebrities.length; i++) {
+    for (var i = 0; i < snlComics.length; i++) {
         var btn = $("<button>");
-        btn.text(celebrities[i]);
-        btn.attr("data-title", celebrities[i]);
+        $("#search-bar").val("");
+        btn.text(snlComics[i]);
+        btn.attr("data-title", snlComics[i]);
         btn.addClass("btn btn-success show-btn");
         $("#buttons").append(btn);
     }
@@ -41,15 +41,22 @@ function makeButtons() {
 makeButtons();
 
 $(document).on("click", ".show-btn", function (event) {
+    event.preventDefault();
     console.log("clicked on a button");
     $("#gif-holder").empty();
-    var celebrities = $(this).attr("data-title");
-    console.log(celebrities);
-    getGifs(celebrities);
+    var snlComics = $(this).attr("data-title");
+    console.log(snlComics);
+    getGifs(snlComics);
 });
 
-$("#gif-holder").on("click", "img", function () {
+$("#gif-holder").on("click", function () {
+    event.preventDefault();
     var imageState = $(this).attr("data-state");
-    console.log(imageState);
-    // var imageState = data-StaticRange.attr()
+    if (imageState === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 });
